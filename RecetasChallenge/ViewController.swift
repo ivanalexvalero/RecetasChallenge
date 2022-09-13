c9ec2bf7bcd75d87bf36b4372cb12777629d86a7
ivan <ivanvalero1985@gmail.com>
2022-09-13 11:39:25 -0300

    Initial Commit

---
+++ RecetasChallenge/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RecetasChallenge
+//
+//  Created by Ivan Valero on 13/09/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
